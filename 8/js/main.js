071731d8cb6ca522f2c25f3b589237bb796fab52
Keks <keks@htmlacademy.ru>
2025-09-15 17:55:06 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,2 @@
+import './render-pictures/render-pictures-list.js';
+import './picture-modal.js';
---
+++ 8/js/picture-modal.js
@@ -0,0 +1,32 @@
+import { picturesList } from './render-pictures/render-pictures-list';
+import { pictureModal, renderPictureModal } from './render-pictures/render-picture-modal';
+import * as handleModal from './render-pictures/handle-picture-modal';
+
+export const closeButtonModal = pictureModal.querySelector('.big-picture__cancel');
+let modalOpen = false;
+
+handleModal.toggleEventModal(modalOpen);
+
+export const showPictureModal = (element) => {
+  const elementList = element.closest('.picture');
+
+  if (!elementList) {
+    return;
+  }
+
+  picturesList.removeEventListener('click', handleModal.handleClickPicturesList);
+  document.removeEventListener('keydown', handleModal.handleOpenModalKeydown);
+
+  renderPictureModal(elementList);
+
+  modalOpen = true;
+  handleModal.toggleEventModal(modalOpen);
+};
+
+export const hidePictureModal = () => {
+  document.removeEventListener('keydown', handleModal.handleCloseKeydown);
+  closeButtonModal.removeEventListener('click', handleModal.handleClickPicturesList);
+
+  modalOpen = false;
+  handleModal.toggleEventModal(modalOpen);
+};
